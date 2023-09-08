b255196e6fc27ab6f3246d594c1a9ea4bda5cfbb
djonesdev <domajones@outlook.com>
2023-09-08 10:16:09 +0100

    f/add logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,16 @@
-import './Header.scss';
+import "./Header.scss";
 
 function Header() {
   return (
-    <header className='header'>
-        <div>
-            This is the header
+    <header className="header">
+      <div className="header__container">
+        <div className="header__logo-container">
+          <img className="header__logo" src="/Pokemon-Logo.png" alt="pokemon-logo" />
         </div>
+        <p className="header__title">Pokedex</p>
+      </div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
