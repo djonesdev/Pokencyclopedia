1887b5303e4ca746653def1830faa9b10bf427fc
djonesdev <domajones@outlook.com>
2023-09-08 20:06:25 +0100

    f/add fallback images to pokecard component

--- src/components/PokemonCard/index.js
+++ src/components/PokemonCard/index.js
@@ -4,6 +4,11 @@ import "./PokemonCard.scss";
 
 const PokemonCard = memo(({ pokemon, getPokemonData }) => {
   const { name, imageUrl, pokedexNumber } = pokemon;
+
+  const addImageFallback = (event) => {
+    event.currentTarget.src = "/PokeGhost.webp";
+  };
+  
   return (
     <button onClick={getPokemonData} className="pokemon-card">
       <div className="pokemon-card__image-container">
@@ -12,6 +17,7 @@ const PokemonCard = memo(({ pokemon, getPokemonData }) => {
           loading="lazy"
           alt={name}
           src={imageUrl}
+          onError={addImageFallback}
         />
       </div>
       <p className="pokemon-card__name">
